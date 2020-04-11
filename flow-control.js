4249dca94a0c17b53216137dd49628a1b7e0927b
Jay Kayshar <jkaywshar@gmail.com>
2020-04-11 15:52:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,8 @@
 function basicTeenager(age)
-describe('basicTeenager', () => {
-  it('should return "You are a teenager!" if the age is between 13-19', () => {
-    expect(basicTeenager(13)).toEqual("You are a teenager!");
-  })
-
+'basicTeenager', (age)=> {13-19}
+if (age) {
+  ("You are a teenager!")
+}
 
 function teenager(age)
 
